84b8d7290bbad19b75b7ae649d7648ad4af19926
Cam <cameron.riemensnider@gmail.com>
2021-03-17 09:06:15 -0700

    Checking to see if db connection successful

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const 
+const sequelize = require('./models');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +22,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection successful!");
+  } catch (error){
+    console.error("Database connection unsuccessful.");
+  }
+})();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
